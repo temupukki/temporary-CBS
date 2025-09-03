d16741ca2a9deb49281391b13a830ae230df812d
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-09-03 23:10:35 +0300

    update

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,23 +1,26 @@
 // app/api/users/[id]/route.ts
-import  prisma  from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 // PATCH /api/users/:id
 export async function PATCH(
   req: Request,
-  { params }: { params: { id: string } }
+  context: { params: { id: string } }
 ) {
   try {
     const body = await req.json();
     const { role } = body;
 
     const updatedUser = await prisma.user.update({
-      where: { id: (params.id) },
+      where: { id: context.params.id },
       data: { role },
     });
 
     return NextResponse.json(updatedUser);
   } catch (error) {
-    return NextResponse.json({ error: "Failed to update user role" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Failed to update user role" },
+      { status: 500 }
+    );
   }
 }
